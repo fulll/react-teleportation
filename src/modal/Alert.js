2d948cb6e92300af37dd3d4f33e751e50735f014
igor13127 <igor.eschalier@gmail.com>
2016-10-24 12:41:03 +0200

    Add Alert component

---
+++ src/modal/Alert.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { capitalize } from 'lodash'
+import styled from 'styled-components'
+
+const fontFamily = 'Noto, Roboto, sans-serif'
+
+const Modal = styled.div`
+  display: table;
+  max-width: 400px;
+  min-width: 350px;
+  height: 164px;
+  margin: calc(50vh - 82px) auto;
+  background-color: white;
+  padding-bottom: 8px;
+`
+const H1 = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+  padding: 24px 24px 0;
+  margin: 0;
+  font-family: ${fontFamily};
+`
+
+const P = styled.p`
+  font-size: 14px;
+  padding: 24px;
+  margin: 0;
+  font-family: ${fontFamily};
+`
+
+const Button = styled.button`
+  background-color: transparent;
+  cursor: pointer;
+  color: #2196F3;
+  font-size: 14px;
+  height: 45px;
+  box-sizing: border-box;
+  font-weight: 500;
+  padding: 12px 10px;
+  float: right;
+  text-transform: uppercase;
+  outline: none;
+  border: none;
+  &:hover {
+    background-color: #eee;
+  }
+`
+
+export const Alert = ({ title, description, right, left }) => (
+  <Modal>
+    <H1>{capitalize(title)}</H1>
+    <P>{capitalize(description)}</P>
+    <footer style={{ padding: 8 }}>
+      <Button onClick={left.action}>{left.title}</Button>
+      <Button onClick={right.action}>{right.title}</Button>
+    </footer>
+  </Modal>
+)
+
+Alert.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string.isRequired,
+  right: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+    action: React.PropTypes.func.isRequired,
+  }),
+  left: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+    action: React.PropTypes.func.isRequired,
+  }),
+}
